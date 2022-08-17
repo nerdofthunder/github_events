cee51b46daada8a829034a1ad33fd50666e55c35
Pat McDermott <coolgeek2751@gmail.com>
2022-08-17 11:36:47 -0400

    Added controller and view for repository events form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'repository_events/index'
+  get 'repository_events/search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'repository_events#index'
 end
